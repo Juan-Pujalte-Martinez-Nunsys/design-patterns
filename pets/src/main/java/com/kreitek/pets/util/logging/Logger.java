9ac495feb39eb2788f711126c335f219448c179e
juan.pujalte <juan.pujalte@nunsys.com>
2025-03-21 14:54:32 +0100

    Logger interface

---
+++ pets/src/main/java/com/kreitek/pets/util/logging/Logger.java
@@ -0,0 +1,13 @@
+package com.kreitek.pets.util.logging;
+
+public interface Logger {
+    void debug(final String message);
+
+    /*
+    void log(final Level level, final String message);
+
+    enum Level {
+        DEBUG
+    }
+    */
+}
